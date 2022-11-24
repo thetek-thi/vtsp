1ffdef54a92fffb9e85ac8d2d0b0509b45c97e52
thetek-thi <git-thi@thetek.de>
2022-11-24 09:37:03 +0100

    p2 2d

--- p2/src/phil-no-multiple-p.c
+++ p2/src/phil-no-multiple-p.c
@@ -1,12 +1,18 @@
 /**
- * Philosopher Problem - Sketch:
+ * If it is not possible to P multiple semaphors at once, such a deadlock might
+ * occur. However, the atomic P operations for multiple semaphors can be
+ * manually implemented by using another semaphor that wraps around the P
+ * operation for the forks like this:
  *
- * Every fork is a semaphor. For taking the fork and putting it back, the
- * standard P and V operations are used. Here, the fact the the IPC semaphors
- * allow for atomic P of multiple semaphors at once comes in handy. If this
- * action would not be atomic, a deadlock might occur (e.g. when each
- * philosopher takes one fork, but cannot take the second one since it is in
- * posession of the next philosopher).
+ *   P(wrapper)
+ *     P(left_fork)
+ *     P(right_fork)
+ *   V(wrapper)
+ *   ...
+ *   V(left_fork)
+ *   V(right_fork)
+ *
+ * Problem: the forks are not being used efficiently.
  */
 
 #include <stdint.h>
@@ -27,7 +33,6 @@
         fprintf (stderr, __VA_ARGS__);          \
         exit (EXIT_FAILURE);                    \
     } while (0);
-#define GETFORKS(i) (i % NUM_PHIL), ((i + 1) % NUM_PHIL)
 
 typedef enum {
     PHILSTATE_EAT,
@@ -52,34 +57,22 @@ void sem_init (int sem, int num) {
     if (semctl (sem, num, SETVAL, arg) < 0) ERR ("semctl");
 }
 
-void sem_p2 (int sem, int n1, int n2) {
-    struct sembuf sembuf[2] = {
-        {
-            .sem_num = n1,
-            .sem_op = SEMOP_P,
-            .sem_flg = ~(IPC_NOWAIT | SEM_UNDO),
-        }, {
-            .sem_num = n2,
-            .sem_op = SEMOP_P,
-            .sem_flg = ~(IPC_NOWAIT | SEM_UNDO),
-        }
+void sem_p (int sem, int n) {
+    struct sembuf sembuf = {
+        .sem_num = n,
+        .sem_op = SEMOP_P,
+        .sem_flg = ~(IPC_NOWAIT | SEM_UNDO),
     };
-    if (semop (sem, sembuf, 2) < 0) ERR ("semop P()");
+    if (semop (sem, &sembuf, 1) < 0) ERR ("semop P()");
 }
 
-void sem_v2 (int sem, int n1, int n2) {
-    struct sembuf sembuf[2] = {
-        {
-            .sem_num = n1,
-            .sem_op = SEMOP_V,
-            .sem_flg = ~(IPC_NOWAIT | SEM_UNDO),
-        }, {
-            .sem_num = n2,
-            .sem_op = SEMOP_V,
-            .sem_flg = ~(IPC_NOWAIT | SEM_UNDO),
-        }
+void sem_v (int sem, int n) {
+    struct sembuf sembuf = {
+        .sem_num = n,
+        .sem_op = SEMOP_V,
+        .sem_flg = ~(IPC_NOWAIT | SEM_UNDO),
     };
-    if (semop (sem, sembuf, 2) < 0) ERR ("semop V()");
+    if (semop (sem, &sembuf, 1) < 0) ERR ("semop V()");
 }
 
 int main (int argc, char **argv) {
@@ -96,9 +89,9 @@ int main (int argc, char **argv) {
     srand (init_time);   // initialize random number generator
 
     if ((key = ftok (*argv, '0')) < 0) ERR ("ftok");
-    if ((sem = semget (key, NUM_PHIL, IPC_CREAT | 0666)) < 0) ERR ("semget");
+    if ((sem = semget (key, NUM_PHIL + 1, IPC_CREAT | 0666)) < 0) ERR ("semget");
 
-    for (i = 0; i < NUM_PHIL; i++)
+    for (i = 0; i <= NUM_PHIL; i++)
         sem_init (sem, i);
 
     for (i = 0; i < NUM_PHIL; i++) {
@@ -114,11 +107,15 @@ int main (int argc, char **argv) {
         if (pid == 0) {
             /* child */
             for (j = 0; j < CYCLES; j++) {
-                sem_p2 (sem, GETFORKS (i));
+                sem_p (sem, NUM_PHIL);
+                sem_p (sem, i);
+                sem_p (sem, (i + 1) % NUM_PHIL);
+                sem_v (sem, NUM_PHIL);
                 phils[i].state = PHILSTATE_EAT;
                 printf ("%3zus: p%zu %s\n", time (0) - init_time, i, phils[i].state == PHILSTATE_EAT ? "eat" : phils[i].state == PHILSTATE_THINK ? "think" : "wait");
                 sleep (phils[i].eat_time);
-                sem_v2 (sem, GETFORKS (i));
+                sem_v (sem, i);
+                sem_v (sem, (i + 1) % NUM_PHIL);
                 phils[i].state = PHILSTATE_THINK;
                 printf ("%3zus: p%zu %s\n", time (0) - init_time, i, phils[i].state == PHILSTATE_EAT ? "eat" : phils[i].state == PHILSTATE_THINK ? "think" : "wait");
                 sleep (phils[i].think_time);
