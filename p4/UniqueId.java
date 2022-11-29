4d772a532d94e1c4e6cbeb34e76811dad6a29483
thetek-thi <git-thi@thetek.de>
2022-11-29 15:42:22 +0100

    p4 2b

--- p4/UniqueId.java
+++ p4/UniqueId.java
@@ -1,8 +1,11 @@
 // Datei: UniqueId.java
 import java.io.*;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class UniqueId {
 	private String file;
+    private ReadWriteLock rwlock = new ReentrantReadWriteLock();
 
 	public UniqueId(String file) {
 		this.file = file;
@@ -15,14 +18,26 @@ public class UniqueId {
 	}		
 
 	public int getNext() throws IOException {
-		DataInputStream in = new DataInputStream(new FileInputStream(file));
-		int oldId = in.readInt();
-		in.close();
+        int oldId, newId;
+
+        rwlock.readLock().lock();
+        try {
+            DataInputStream in = new DataInputStream(new FileInputStream(file));
+            oldId = in.readInt();
+            in.close();
+        } finally {
+            rwlock.readLock().unlock();
+        }
 		
-		DataOutputStream out = new DataOutputStream(new FileOutputStream(file));
-		int newId = ++oldId;
-		out.writeInt(newId);
-		out.close();
+        rwlock.writeLock().lock();
+        try {
+            DataOutputStream out = new DataOutputStream(new FileOutputStream(file));
+            newId = ++oldId;
+            out.writeInt(newId);
+            out.close();
+        } finally {
+            rwlock.writeLock().unlock();
+        }
 
 		return newId;
 	}
