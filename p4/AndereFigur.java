dd9065bfa00b64c240b336452efbf2f77f8a987e
thetek-thi <git-thi@thetek.de>
2022-11-29 15:26:58 +0100

    p4 1c

--- p4/AndereFigur.java
+++ p4/AndereFigur.java
@@ -1,12 +1,27 @@
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class AndereFigur extends Figur {
+    private ReadWriteLock rwlock = new ReentrantReadWriteLock();
+
     public void setPosition(char x, int y) {
-        this.x = x;
-        MachMal.eineZehntelSekundeLangGarNichts();
-        this.y = y;
+        rwlock.writeLock().lock();
+        try {
+            this.x = x;
+            MachMal.eineZehntelSekundeLangGarNichts();
+            this.y = y;
+        } finally {
+            rwlock.writeLock().unlock();
+        }
     }
 
     public String getPosition() {
-        MachMal.eineSekundeLangGarNichts();
-        return "(" + x + ", " + y + ")";
+        rwlock.readLock().lock();
+        try {
+            MachMal.eineSekundeLangGarNichts();
+            return "(" + x + ", " + y + ")";
+        } finally {
+            rwlock.readLock().unlock();
+        }
     }
 }
