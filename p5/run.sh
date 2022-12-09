989937373def92c7ac2e2e6b0ab1557bdba51b26
Some Student. Put your name here <your.email@your.provider>
2022-12-09 09:57:44 +0100

    p5: rmiregistry debug messages

--- p5/run.sh
+++ p5/run.sh
@@ -3,7 +3,7 @@ set -xe
 
 javac *.java
 case "$1" in
-  rmireg) rmiregistry;;
+  rmireg) rmiregistry -J-Djava.rmi.server.logCalls=true;;
   server) java DaytimeServer;;
   client) java DaytimeClient $2;;
 esac
