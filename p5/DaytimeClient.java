e3995543b556440a1c148ab6d28f507f66324433
thetek-thi <git-thi@thetek.de>
2022-12-14 14:44:44 +0100

    p5: more times

--- p5/DaytimeClient.java
+++ p5/DaytimeClient.java
@@ -1,11 +1,14 @@
 import java.rmi.Naming;
+import java.time.Instant;
 
 public class DaytimeClient {
   public static void main(String args[]) throws Exception {
     String host = args[0];
 
+    System.out.println("(before request) " + Instant.now().toString());
     Daytime remote = (Daytime) Naming.lookup("//" + host + "/daytime");
     String recvd = remote.getDaytime();
-    System.out.println(recvd);
+    System.out.println("(from request)   " + recvd);
+    System.out.println("(after request)  " + Instant.now().toString());
   }
 }
